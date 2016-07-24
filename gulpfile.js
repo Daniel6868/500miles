eaa1b792222ce944131bf98d1989f4b5f578d9d8
debayansen7 <debayan.sen7@gmail.com>
2016-07-24 21:04:33 +0530

    500miles created

---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+// 'use strict';
+
+var gulp    = require('gulp'),
+    order   = require('gulp-order'),
+    concat  = require('gulp-concat'),
+    uglify  = require('gulp-uglify'),
+    sass    = require('gulp-sass'),
+    rename  = require('gulp-rename'),
+    sourcemaps   =  require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    watch   = require('gulp-watch')
+;
+
+// --------------------------------------------------------------------
+
+//For get-min-scripts
+gulp.task('getminscripts', function(){
+    gulp.src('bower_components/**/*.min.js').pipe(gulp.dest('src/js/lib/'));
+});
+//For min-scripts
+gulp.task('minscripts', function(){
+    gulp.src('bower_components/**/*.min.js')
+        .pipe(concat('lib.js'))
+        .pipe(gulp.dest('src/js/'));
+});
+
+//For Scripts
+gulp.task('scripts', function(){
+    gulp.src('src/js/*.js')
+        .pipe(order([
+            "main.js"
+        ]))
+        // .pipe(uglify())
+        .pipe(concat('main.js'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('src/js/'));
+});
+
+// --------------------------------------------------------------------
+
+//For get-min-styles
+gulp.task('getminstyles', function(){
+    gulp.src('bower_components/**/*.min.css')
+        // .pipe(order([
+        //     "angular-material.min.css"
+        // ]))
+        .pipe(concat('lib.css'))
+        .pipe(gulp.dest('src/css/'))
+        // .pipe(browserSync.stream())
+        ;
+});
+//For min-styles
+gulp.task('minstyles', function(){
+    // gulp.src('bower_components/**/*.css')
+    gulp.src('src/js/lib/*.min.css')
+        .pipe(order([
+            "angular-material.min.css"
+        ]))
+        .pipe(concat('lib.css'))
+        .pipe(gulp.dest('src/css/'))
+        // .pipe(browserSync.stream())
+        ;
+});
+
+//For Styles
+gulp.task('styles', function(){
+    gulp.src('src/scss/style.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        // .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('src/css/'));
+});
+
+//For Watch Task
+gulp.task('watch', function(){
+    gulp.watch('src/scss/**/*.scss',['styles']);
+    gulp.watch('src/js/**/*.js',['scripts']);
+});
+
+//For Default Task
+gulp.task('default', ['getminscripts','getminstyles','minstyles','minscripts','styles','scripts','watch']);
